275e04d437d2a463c468a7917eef40811f183f01
Megan Campbell <megan.campbell26@yahoo.com>
2020-08-21 17:48:52 -0600

    Add intialize test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,12 @@
 class Deck
-  attr_reader :card
+  attr_reader :cards
 
-  def initialize
-    @cards = card
+  def initialize(cards)
+    @cards = cards
   end
 
-  def rank_of_card_at
+  def rank_of_card_at(rank)
+
   end
 
   def high_ranking_cards
