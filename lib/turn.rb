2f917e33b55f3ce6f64d30d43a6986b3cee225d1
Megan Campbell <megan.campbell26@yahoo.com>
2020-08-22 20:40:17 -0600

    Add turn test

---
+++ lib/turn.rb
@@ -0,0 +1,6 @@
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+
+  def
+
+end
---
+++ test/turn_test.rb
@@ -0,0 +1,14 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'minitest/autorun'
+require 'minitest/pride'
+require pry
+
+class TurnTest < Minitest::Test
+  def test_does_it_initialize
+    
+  end
+
+end
